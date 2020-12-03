0600b7ff26b1f2800aa74ded6cc554cfe85af953
anyabot <52899201+anyabot@users.noreply.github.com>
2020-12-03 21:59:39 +0900

    Test

--- src/test/java/simplejavacalculator/TestCalculator.java
+++ src/test/java/simplejavacalculator/TestCalculator.java
@@ -1,4 +1,4 @@
-package simplejavacalculatorTest;
+package simplejavacalculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
