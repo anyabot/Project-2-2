77f7ecf754aa96230173c9aa22f7db554959480e
anyabot <52899201+anyabot@users.noreply.github.com>
2020-12-04 23:30:48 +0900

    fix test

--- src/main/java/simplejavacalculator/Calculator.java
+++ src/main/java/simplejavacalculator/Calculator.java
@@ -15,14 +15,45 @@ import static java.lang.Math.pow;
 public class Calculator {
 
     public enum BiOperatorModes {
-        normal, add, minus, multiply, divide , xpowerofy 
+        /**
+         * Operations that requires two operands.
+         * normal return num2
+         * add return the product of num1 and num2
+         * minus return the difference of num1 and num2
+         * multiply return the product of num1 and num2
+         * divide return the quotient of num1 and num2
+         * xpowerofy return num1 to the power of num2
+         */
+        normal, add, minus, multiply, divide, xpowerofy
     }
 
     public enum MonoOperatorModes {
-        square, squareRoot, oneDevidedBy, cos, sin, tan ,log , rate, abs
+        /**
+         * Operations that requires one operand.
+         * square return the square of num
+         * squareRoot return the square root of num
+         * oneDevidedBy return the quotient of 1 and num
+         * cos return the cosine of num (in degree)
+         * sin return the sine of num (in degree)
+         * tan return the tangent of num (in degree)
+         * log return the logarithm of num to base 10
+         * rate return the quotient of num and 100
+         * abs return the absolute value of num
+         */
+        square, squareRoot, oneDevidedBy, cos, sin, tan, log, rate, abs
     }
 
-    private Double num1, num2;
+    /**
+     * num1 is the first operand of a two-operand operation.
+     */
+    private Double num1;
+    /**
+     * num2 is the second operand of a two-operand operation.
+     */
+    private Double num2;
+    /**
+     * BiOperatorModes is the operation to be used.
+     */
     private BiOperatorModes mode = BiOperatorModes.normal;
 
     private Double calculateBiImpl() {
@@ -46,14 +77,21 @@ public class Calculator {
             return num1 / num2;
         }
         if (mode == BiOperatorModes.xpowerofy) {
-            return pow(num1,num2);
+            return pow(num1, num2);
         }
 
         // never reach
         throw new Error();
     }
 
-    public Double calculateBi(BiOperatorModes newMode, Double num) {
+    /**
+     * calculateBi return the result of a operation that requires two operands.
+     * @return result of newMode operation
+     * @param newMode type of operation
+     * @param num second operand
+     */
+
+    public Double calculateBi(final BiOperatorModes newMode, final Double num) {
         if (mode == BiOperatorModes.normal) {
             num2 = 0.0;
             num1 = num;
